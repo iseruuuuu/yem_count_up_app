ad0b2388d362e1680e8580a81190920eeda84d2e
iseruuuuu <ryutaro.iseki23@gmail.com>
2022-06-09 12:10:43 +0900

    カウントアップのアプリを作成してみた

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 # yem_todo_app
 
-# プロジェクト作成
+# 作成の流れ
 
-１、`cargo install wasm-pack`  
-２、`cargo new --lib yew-app && cd yew-app`  
+１、`cargo install wasm-pack`    
+２、`cargo new --lib yew-app && cd yew-app`    
+３、`lib.rs`に記載する    
+４、`index.html`に記載する  
+５、cargoのツールを使用してサーバを立ち上げる[`wasm-pack build --target web --out-name wasm --out-dir ./static`]  
+６、Rust ツールチェイン(初回時のみ？？)[`curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh`]  
+7、`rustup toolchain install nightly --allow-downgrade`を打ち込む（何のコマンドかわからない）    
+8、 `rustup toolchain install stable-x86_64-unknown-linux-gnu --allow-downgrade`（何のコマンドかわからない）    
+9、`cargo +nightly install miniserve`  
+10、`miniserve ./static --index index.html`  
 
 
 ## Yewとは
@@ -19,9 +27,8 @@
 
 
 ## WebAssemblyとは
-->C言語などの高級言語をブラウザ上で実行可能なバイナリ形式にする技術  
-->直接WASMをさわることはなく、C言語やRustなどでコードを実装し、それらがWASMバイトコードにコンパイルされます。  
-
-  ->コンパイルされたコードはWeb上で実行され、ネイティブマシンコードに変換されるため高速で動く、といった感じ  
+->C言語などの高級言語をブラウザ上で実行可能なバイナリ形式にする技術    
+->直接WASMをさわることはなく、C言語やRustなどでコードを実装し、それらがWASMバイトコードにコンパイルされます。    
+->コンパイルされたコードはWeb上で実行され、ネイティブマシンコードに変換されるため高速で動く、といった感じ    
 
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,47 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use wasm_bindgen::prelude::*;
+use yew::prelude::*;
+
+struct Model {
+    link: ComponentLink<Self>,
+    value: i64,
+}
+
+enum Msg {
+    AddOne,
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Self { link, value: 0 }
     }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::AddOne => self.value += 1,
+        }
+        true
+    }
+
+    //新しいプロパティが以前に受信したプロパティと異なる場合にのみ、"true "を返すべきである。
+    //このコンポーネントはプロパティを持たないので、常に "false "を返すことになる。
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <div>
+            <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
+            <p>{ self.value }</p>
+        </div>
+        }
+    }
+}
+
+#[wasm_bindgen(start)]
+pub fn run_app() {
+    App::<Model>::new().mount_to_body();
 }
